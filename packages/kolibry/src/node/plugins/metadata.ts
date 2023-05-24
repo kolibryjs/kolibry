444fbfdd42db7f806db00b83f925f03ea3129812
nyxb <contact@nyxb.xyz>
2023-05-24 22:45:36 +0200

    🚀 feat(define.ts): add definePlugin to replace process.env and import.meta.env variables with user-defined values The definePlugin replaces process.env and import.meta.env variables with user-defined values. It also adds support for import.meta.hot and import.meta.env.SSR. The plugin generates a pattern to match the variables and replaces them with the corresponding values. The plugin also adds support for server-side rendering (SSR) by generating a separate pattern for SSR. The plugin is only applied to JavaScript files and is excluded from HTML, CSS, and static assets.

---
+++ packages/kolibry/src/node/plugins/metadata.ts
@@ -0,0 +1,18 @@
+import type { Plugin } from '../plugin'
+
+/**
+ * Prepares the rendered chunks to contain additional metadata during build.
+ */
+export function metadataPlugin(): Plugin {
+  return {
+    name: 'kolibry:build-metadata',
+
+    async renderChunk(_code, chunk) {
+      chunk.kolibryMetadata = {
+        importedAssets: new Set(),
+        importedCss: new Set(),
+      }
+      return null
+    },
+  }
+}
