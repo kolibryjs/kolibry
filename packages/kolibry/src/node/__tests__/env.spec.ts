85369cd8f8bdd4b7f58425e6a1f346e63c0cf579
nyxb <contact@nyxb.xyz>
2023-05-24 22:33:14 +0200

    ✨ feat(config.spec.ts): add tests for mergeConfig and resolveEnvPrefix functions This commit adds tests for the mergeConfig and resolveEnvPrefix functions in the config.spec.ts file. The mergeConfig function is tested for handling configs with different alias schemas, keeping object alias schema, handling arrays, handling assetsInclude, not handling alias not under `resolve`, and merging arrays correctly. The resolveEnvPrefix function is tested for using 'KOLIBRY_' as default value, throwing an error if envPrefix contains '', and working correctly for valid envPrefix value. Additionally, tests for preview config and resolveConfig functions are added. The preview config tests check if preview inherits server config with default port, preview inherits server config with port override, and preview overrides server config. The resolveConfig tests check if plugin merges `clearScreen` option and plugin overrides `clearScreen` option.

---
+++ packages/kolibry/src/node/__tests__/env.spec.ts
@@ -0,0 +1,73 @@
+import { join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { describe, expect, test } from 'vitest'
+import { loadEnv } from '../env'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+describe('loadEnv', () => {
+  test('basic', () => {
+    expect(loadEnv('development', join(__dirname, './env')))
+      .toMatchInlineSnapshot(`
+        {
+          "KOLIBRY_APP_BASE_ROUTE": "/",
+          "KOLIBRY_APP_BASE_URL": "/",
+          "KOLIBRY_ENV1": "ENV1",
+          "KOLIBRY_ENV2": "ENV2",
+          "KOLIBRY_ENV3": "ENV3",
+        }
+      `)
+  })
+
+  test('specific prefix', () => {
+    expect(loadEnv('development', join(__dirname, './env'), 'VKOLIBRY'))
+      .toMatchInlineSnapshot(`
+        {
+          "VKOLIBRY_A": "A",
+          "VKOLIBRY_B": "B",
+        }
+      `)
+  })
+
+  test('override', () => {
+    expect(loadEnv('production', join(__dirname, './env')))
+      .toMatchInlineSnapshot(`
+        {
+          "KOLIBRY_APP_BASE_ROUTE": "/app/",
+          "KOLIBRY_APP_BASE_URL": "/app/",
+        }
+      `)
+  })
+
+  test('KOLIBRY_USER_NODE_ENV', () => {
+    loadEnv('development', join(__dirname, './env'))
+    expect(process.env.KOLIBRY_USER_NODE_ENV).toEqual(undefined)
+  })
+
+  test('KOLIBRY_USER_NODE_ENV for dev behaviour in build', () => {
+    const _nodeEnv = process.env.NODE_ENV
+    process.env.NODE_ENV = 'production'
+    loadEnv('testing', join(__dirname, './env'))
+    expect(process.env.KOLIBRY_USER_NODE_ENV).toEqual('development')
+    process.env.NODE_ENV = _nodeEnv
+  })
+
+  test('Already exists KOLIBRY_USER_NODE_ENV', () => {
+    process.env.KOLIBRY_USER_NODE_ENV = 'test'
+    loadEnv('development', join(__dirname, './env'))
+    expect(process.env.KOLIBRY_USER_NODE_ENV).toEqual('test')
+  })
+
+  test('prioritize existing process.env', () => {
+    process.env.KOLIBRY_ENV_TEST_ENV = 'EXIST'
+    expect(loadEnv('existing', join(__dirname, './env')))
+      .toMatchInlineSnapshot(`
+        {
+          "KOLIBRY_APP_BASE_ROUTE": "/",
+          "KOLIBRY_APP_BASE_URL": "/",
+          "KOLIBRY_ENV_TEST_ENV": "EXIST",
+          "KOLIBRY_USER_NODE_ENV": "test",
+        }
+      `)
+  })
+})
