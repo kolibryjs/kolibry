82f035926ca413b095f5012bdf47ee5fcdca2bf0
nyxb <contact@nyxb.xyz>
2023-05-24 22:26:48 +0200

    🚀 feat(kolibry.js): add support for debugging and profiling This commit adds support for debugging and profiling to the kolibry CLI. The `--debug` flag can be used to enable debugging, and the `--filter` flag can be used to filter debug output. The `--profile` flag can be used to enable profiling, which uses the `node:inspector` module to start a profiling session.

---
+++ packages/kolibry/bin/kolibry.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+import { performance } from 'node:perf_hooks'
+
+if (!import.meta.url.includes('node_modules')) {
+  try {
+    // only available as dev dependency
+    await import('source-map-support').then((r) => r.default.install())
+  } catch (e) {}
+}
+
+global.__kolibry_start_time = performance.now()
+
+// check debug mode first before requiring the CLI.
+const debugIndex = process.argv.findIndex((arg) => /^(?:-d|--debug)$/.test(arg))
+const filterIndex = process.argv.findIndex((arg) =>
+  /^(?:-f|--filter)$/.test(arg),
+)
+const profileIndex = process.argv.indexOf('--profile')
+
+if (debugIndex > 0) {
+  let value = process.argv[debugIndex + 1]
+  if (!value || value.startsWith('-')) {
+    value = 'kolibry:*'
+  } else {
+    // support debugging multiple flags with comma-separated list
+    value = value
+      .split(',')
+      .map((v) => `kolibry:${v}`)
+      .join(',')
+  }
+  process.env.DEBUG = `${
+    process.env.DEBUG ? process.env.DEBUG + ',' : ''
+  }${value}`
+
+  if (filterIndex > 0) {
+    const filter = process.argv[filterIndex + 1]
+    if (filter && !filter.startsWith('-')) {
+      process.env.KOLIBRY_DEBUG_FILTER = filter
+    }
+  }
+}
+
+function start() {
+  return import('../dist/node/cli.js')
+}
+
+if (profileIndex > 0) {
+  process.argv.splice(profileIndex, 1)
+  const next = process.argv[profileIndex]
+  if (next && !next.startsWith('-')) {
+    process.argv.splice(profileIndex, 1)
+  }
+  const inspector = await import('node:inspector').then((r) => r.default)
+  const session = (global.__kolibry_profile_session = new inspector.Session())
+  session.connect()
+  session.post('Profiler.enable', () => {
+    session.post('Profiler.start', start)
+  })
+} else {
+  start()
+}
