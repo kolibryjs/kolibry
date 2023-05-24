e4b5c580297203da2e651cbae2f0b880d6eef464
nyxb <contact@nyxb.xyz>
2023-05-24 22:27:19 +0200

    🆕 chore(prePatchTypes.ts): add prePatchTypes script to rewrite imports of dep-types/* to relative imports This script walks through the temp dts directory, finds all import/export of dep-types/*, and rewrites them into relative imports. This is done so that api-extractor includes them in the rolled-up final d.ts file. The script uses the rewriteImports function from util.ts to perform the import rewriting. Once the rewriting is done, a message is logged to the console indicating that the imports have been patched.

---
+++ packages/kolibry/scripts/prePatchTypes.ts
@@ -0,0 +1,24 @@
+import { dirname, relative, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import colors from '@nyxb/picocolors'
+import { rewriteImports, slash } from './util'
+import consolji from 'consolji'
+
+const dir = dirname(fileURLToPath(import.meta.url))
+const tempDir = resolve(dir, '../temp/node')
+const depTypesDir = resolve(dir, '../src/types')
+
+// walk through the temp dts dir, find all import/export of, deps-types/*
+// and rewrite them into relative imports - so that api-extractor actually
+// includes them in the rolled-up final d.ts file.
+rewriteImports(tempDir, (importPath, currentFile) => {
+  if (importPath.startsWith('dep-types/')) {
+    const absoluteTypePath = resolve(
+      depTypesDir,
+      importPath.slice('dep-types/'.length),
+    )
+    return slash(relative(dirname(currentFile), absoluteTypePath))
+  }
+})
+
+consolji.log(colors.nicegreen(colors.bold(`patched deps-types/* imports`)))
